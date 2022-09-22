2b72ce8a3b061a2ee0ce564a083bba229d047d81
Holly Hoffman <hoffh5334@gmail.com>
2022-09-21 20:53:16 -0500

    added global variables and timer in JS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
-//selecting Dom Elements
+//Global variables
+var countdown = 120;
+var score = 0;
+var i = 0;
 
+
+
+//selecting Dom Elements
 var startButton = document.querySelector("#startButton");
 var timer = document.querySelector("#timer");
 var header = document.querySelector("header");
@@ -10,3 +16,34 @@ var inputButton = document.createElement("button");
 var viewHighScore = document.createElement("button");
 var playAgain = document.createElement("button");
 
+
+
+
+
+//updates countdown timer
+function setTime() {
+  var timerInterval = setInterval(function () {
+    countdown--;
+    console.log(countdown);
+    if (countdown === 0 || countdown < 0) {
+      clearInterval(timerInterval);
+      countdown = 0;
+      endGame();
+    }
+    timer.textContent = countdown;
+
+  }, 1000);
+}
+
+
+
+// Start Button that initiates everything
+
+startButton.addEventListener("click", function () {
+  header.innerHTML = ""             //important to clear, 
+  setTime(); //a timer starts 
+  askingQuestionX();
+  //retrieveHighScore() ; //Not pushing to the array
+
+
+});
