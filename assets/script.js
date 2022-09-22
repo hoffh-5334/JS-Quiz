ed0bb27f6df3c19c520474b23d997044f66355c4
Holly Hoffman <hoffh5334@gmail.com>
2022-09-21 20:20:41 -0500

    re-worked html and selected DOM elements in JS

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//selecting Dom Elements
+
+var startButton = document.querySelector("#startButton");
+var timer = document.querySelector("#timer");
+var header = document.querySelector("header");
+var main = document.querySelector("main");
+var currentScore = document.querySelector("#score");
+var saveScore = document.createElement("highScore");
+var inputButton = document.createElement("button");
+var viewHighScore = document.createElement("button");
+var playAgain = document.createElement("button");
+
